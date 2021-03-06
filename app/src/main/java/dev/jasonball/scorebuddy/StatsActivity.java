336df8b2b2ce1efbe4b897f59b1baf76ee367f09
andre <andre.austin98@gmail.com>
2021-03-06 00:14:19 -0500

    stats page

--- app/src/main/java/dev/jasonball/scorebuddy/StatsActivity.java
+++ app/src/main/java/dev/jasonball/scorebuddy/StatsActivity.java
@@ -3,6 +3,7 @@ package dev.jasonball.scorebuddy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class StatsActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class StatsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
+
+        Button b1 = findViewById(R.id.back_home);
     }
 }
